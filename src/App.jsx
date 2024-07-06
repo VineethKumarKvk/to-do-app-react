789206f94b05cfb5927c7926bec232604462f02a
vineeth <vineethkumar0005@gmail.com>
2024-07-06 18:56:34 +0530

    Second Commit - With cookies In this version tasks will be saved for one day.After that they will be deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
-import './App.css'
-import JustList from './components/JustList'
-import Header from './components/Header'
-import TextInput from './components/TextInput'
-import Button from './components/Button'
-import { useState } from 'react'
+import './App.css';
+import JustList from './components/JustList';
+import Header from './components/Header';
+import TextInput from './components/TextInput';
+import Button from './components/Button';
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+const TODO_COOKIE = 'todoList';
 
 function App() {
-  const [inputValue,setInputValue] = useState('')
-  const [listValue,setListValue] = useState([])
+  const [inputValue, setInputValue] = useState('');
+  const [listValue, setListValue] = useState(() => {
+    const savedList = Cookies.get(TODO_COOKIE);
+    return savedList ? JSON.parse(savedList) : [];
+  });
+
+  useEffect(() => {
+    Cookies.set(TODO_COOKIE, JSON.stringify(listValue), { expires: 7 });
+  }, [listValue]);
+
   function inputChangeHandler(event) {
-    setInputValue(event.target.value)
-    console.log(inputValue);
+    setInputValue(event.target.value);
   }
 
-  function clickHandler(inputValue) {
-    console.log("Button is clicked")
-    if(inputValue) {
+  function clickHandler() {
+    console.log('Button is clicked');
+    if (inputValue) {
       setListValue(currentList => {
         const newList = [...currentList, inputValue];
-        console.log(inputValue + " Pushed");
         return newList;
-      })
+      });
+      setInputValue('');
     }
-    setInputValue('')
   }
+
   return (
     <>
-    <header>
-      <Header/>
-    </header>
-    <main>
-      <TextInput value={inputValue} onChange={inputChangeHandler}/>
-      <Button onClick={()=>clickHandler(inputValue)}>Add</Button>
-      <JustList items={listValue}/>
-    </main>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <TextInput value={inputValue} onChange={inputChangeHandler} />
+        <Button onClick={clickHandler}>Add</Button>
+        <JustList items={listValue} /> {/* Use listValue instead of LIST_TO_DO */}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/JustList.jsx
+++ src/components/JustList.jsx
@@ -1,32 +1,49 @@
-import { useState } from "react";
-import './JustList.css'
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import './JustList.css';
 
-function JustList({items}) {
-  const [check,setCheck] = useState([]);
+const CHECKED_ITEMS_COOKIE = 'checkedItems';
+
+function JustList({ items }) {
+  const [check, setCheck] = useState(() => {
+    const savedCheckedItems = Cookies.get(CHECKED_ITEMS_COOKIE);
+    return savedCheckedItems ? JSON.parse(savedCheckedItems) : [];
+  });
+
+  useEffect(() => {
+    Cookies.set(CHECKED_ITEMS_COOKIE, JSON.stringify(check), { expires: 1 });
+  }, [check]);
 
   function changeHandler(selectedItem) {
     setCheck(currentList => {
-      if(currentList.length && currentList.some(cl => cl === selectedItem)) {
-        return [...currentList].filter(li => li!=selectedItem)
-      }
-      else {
-        return [...currentList,selectedItem]
+      if (currentList.some(cl => cl === selectedItem)) {
+        return currentList.filter(li => li !== selectedItem);
+      } else {
+        return [...currentList, selectedItem];
       }
     });
-    console.log(selectedItem + "Changed")
-  }
-  console.log('Loaded with'+check)
-    return (
-      <div>
-        {items.map(
-          (item,index) =>
-            <div key={index}>
-              <input onChange={() => changeHandler(item)} type="checkbox" value={item}/>
-              <label className={check.some((l) => l === item)?"strikethrough":undefined}>{item}</label>
-            </div>
-          )}
-      </div>
-    );
+    console.log(selectedItem + " Changed");
   }
 
-  export default JustList;
+  console.log('Loaded with ' + check);
+
+  return (
+    <div>
+      {items.map((item, index) => (
+        <div key={index}>
+          <input
+            onChange={() => changeHandler(item)}
+            type="checkbox"
+            checked={check.includes(item)}
+            value={item}
+          />
+          <label className={check.includes(item) ? "strikethrough" : undefined}>
+            {item}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default JustList;
